f4c9f15ccf20faf49a00f2b1d0fdb2eb3b3221dd
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-04-24 14:55:21 +0900

    8

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -23,7 +23,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!!123 nice weather!";
+        return "I'm alive!!123 nice weather! 내가 이겨따!!!";
     }
 
 //    @GetMapping("/plus")
